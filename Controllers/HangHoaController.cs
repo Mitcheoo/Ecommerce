9d15ede495bdd0fa14824b58db404f8a03088cd3
zizindz2607 <108909848+zizindz2607@users.noreply.github.com>
2025-10-03 22:02:24 +0700

    04/10Add project files.

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Data;
+using Ecommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly DatabaseEcommerceContext db;
+
+        public HangHoaController(DatabaseEcommerceContext context) { db = context; }
+        public IActionResult Index(int? Loai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if(Loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == Loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia ?? 0,
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+    }
+}
