94dc90a4d440c5f509d7bd9fa4b0800b16201187
zizindz2607 <108909848+zizindz2607@users.noreply.github.com>
2025-10-03 22:02:24 +0700

    04/10Add project files.

---
+++ ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ecommerce.Data;
+
+namespace Ecommerce.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly DatabaseEcommerceContext db;
+
+        public MenuLoaiViewComponent(DatabaseEcommerceContext context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new Ecommerce.ViewModels.MenuLoaiVM
+            {
+                MaLoai = lo.MaLoai,
+               TenLoai = lo.TenLoai,
+                SoLuong = lo.HangHoas.Count
+            }).OrderBy(p=>p.TenLoai);
+            return View(data); //default.cshtml dsdasdasdasdasdsd
+            //abc
+
+        }
+    }
+}
